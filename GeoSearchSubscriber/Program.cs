999642df9a086e09ee2164a1859232990fb86b63
anovak57 <ante.novak408@gmail.com>
2024-12-17 01:39:41 +0100

    Create simple console app subscriber
    - Subscribes to SignalR hub
    - Prints in the console all the search requests

---
+++ GeoSearchSubscriber/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting SignalR client...");
+        
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.development.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var hubUrl = configuration["SignalR:HubUrl"]!;
+        var apiKey = configuration["SignalR:ApiKey"]!;
+
+        var connection = new HubConnectionBuilder()
+            .WithUrl(hubUrl, options =>
+            {
+                options.Headers.Add("x-api-key", apiKey);
+            })
+            .Build();
+
+        connection.On<LocationSearchRequest>("ReceiveSearchRequest", searchRequest =>
+        {
+            Console.WriteLine($"\nReceived search request: \n" +
+                              $"--Latitude: {searchRequest.Latitude},\n" +
+                              $"--Longitude: {searchRequest.Longitude},\n" +
+                              $"--Query: {searchRequest.Query},\n" +
+                              $"--Radius: {searchRequest.Radius}\n" +
+                              $"==================================");
+        });
+
+        try
+        {
+            await connection.StartAsync();
+            Console.WriteLine("Connected to SignalR hub.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error connecting to hub: {ex.Message}");
+            return;
+        }
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
+
+public class LocationSearchRequest
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Query { get; set; }
+    public int Radius { get; set; }
+}
